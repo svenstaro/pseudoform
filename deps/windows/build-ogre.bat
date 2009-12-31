1ad6acbd4d4f9f4260c9b5efc1e3184d498397f0
Sven-Hendrik Haase <sh@lutzhaase.com>
2009-12-31 08:51:48 +0100

    Windows devkit: Missing directory for Ogre now created

--- deps/windows/build-ogre.bat
+++ deps/windows/build-ogre.bat
@@ -6,6 +6,7 @@ cd ogre
 copy ..\bin\* Samples\Common\bin\Release\
 mv Samples\Common\bin\Release\OIS_d.dll Samples\Common\bin\Debug\
 cp Samples\Common\bin\Release\cg.dll Samples\Common\bin\Debug\
+mkdir Dependencies
 cp -r ../lib Dependencies/
 cp -r ../include Dependencies/
 mkdir build
